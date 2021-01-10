0d70707f7e5decd3039d5aba3d8f7f04ace0ac4a
unknown <tristan.e.perera@gmail.com>
2021-01-09 19:15:53 -0800

    added pricing page

--- server.js
+++ server.js
@@ -4,47 +4,51 @@ const http = require('http');
 
 // Setting port
 const port = process.env.PORT || 5000;
-const host = process.env.HOST || "192.168.1.35"
+const host = process.env.HOST || "192.168.1.19"
 
 // Start Express Instance
 const app = express();
 
 // Body parsing middleware; Set max file size to 10mb
 app.use(express.json({ limit: '1mb' }));
-// app.use(express.urlencoded({ limit: '10mb' }));
-
 
 // Serve static assets if in production
-// if (process.env.NODE_ENV === 'production') {
 // Set static folder
 app.use(express.static("html"))
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'html', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'pages', 'index.html'));
 })
 app.get("/services", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'html', 'services.html'));
+    res.sendFile(path.resolve(__dirname, 'pages', 'services.html'));
+})
+app.get("/pricing", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages', 'pricing.html'));
 })
 app.get("/portfolio", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'html', 'portfolio.html'));
+    res.sendFile(path.resolve(__dirname, 'pages', 'portfolio.html'));
 })
 app.get("/contact", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'html', 'contact-us.html'));
+    res.sendFile(path.resolve(__dirname, 'pages', 'contact-us.html'));
 })
 app.get("/privacy", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'html', 'privacy.html'));
+    res.sendFile(path.resolve(__dirname, 'pages', 'privacy.html'));
 })
 app.get("/terms", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'html', 'terms.html'));
+    res.sendFile(path.resolve(__dirname, 'pages', 'terms.html'));
 })
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'html', '404.html'));
-    // res.json({ status: 404, message: "404 not found" })
+    res.sendFile(path.resolve(__dirname, 'pages', '404.html'));
 })
-// } else {
-// }
+
+app.get("/.well-known/acme-challenge/6kvagXsNn3b26Fk3Ct_reGY-eT-taaMQ79uEaTg3Vk0", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "html", ".well-known", "acme-challenge", "6kvagXsNn3b26Fk3Ct_reGY-eT-taaMQ79uEaTg3Vk0", "6kvagXsNn3b26Fk3Ct_reGY-eT-taaMQ79uEaTg3Vk0.tKxqudkAHjbp2TFTLzzTlukU3yKW8IKP4NQ2r56Q_lY"));
+})
+app.get("/.well-known/acme-challenge/LhpsAeu61dm_SA97zY9-aGouynsIRE0-agZrDWdU14w", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "html", ".well-known", "acme-challenge", "LhpsAeu61dm_SA97zY9-aGouynsIRE0-agZrDWdU14w", "LhpsAeu61dm_SA97zY9-aGouynsIRE0-agZrDWdU14w.tKxqudkAHjbp2TFTLzzTlukU3yKW8IKP4NQ2r56Q_lY"));
+})
+
 
 // Start Node server and listen on port
-
 if (process.env.NODE_ENV === 'production') {
     http.createServer(app).listen(port, host, () => console.log("Node Server started on port: " + host + ":" + port))
 } else {
